b947da0287ca91dbd80e2f4b66f73758e81ea03f
jiweiqi <jonhnstein@qq.com>
2021-01-29 18:34:42 -0500

    update robertson's demo

--- robertson/ReadMe.md
+++ robertson/ReadMe.md
@@ -1,3 +1,14 @@
 # Robertson's Problem
 
-This example demonstrate the preliminary results on learning CRNN for systems with strong stiffness. Robertson's problem is a classical stiff chemical kinetic problems widely adopted to test stiff ODE solvers. The details of Robertson's problem and the inttroductions to stiffness can be found in https://arxiv.org/abs/2011.04520 and https://diffeq.sciml.ai/stable/tutorials/advanced_ode_example/.
+This example demonstrate the preliminary results on learning CRNN for systems with strong stiffness. Robertson's problem is a classical stiff chemical kinetic problems widely adopted to test stiff ODE solvers. The details of Robertson's problem and the introductions to stiffness can be found in https://arxiv.org/abs/2011.04520 and https://diffeq.sciml.ai/stable/tutorials/advanced_ode_example/.
+
+We are actively working on understanding the difficulties of learning stiff Neural Ordinary Differential Equations. We will phase out a nice story on it soon.
+
+For this Robertson's problem, a note on the number of proposed reactions is that proposing a onver-parameterized model seems to be easier to optimize. For instance, we know this system can be well approximated by a three-reaction systems. However, it could be difficult to optimize due to local minima. Instead, if we propose 6 reactions, the chaance of successful optimization is much higher. To get a sparse model for better interpretability, we could employ standard model reduction methods to identify essential pathways. For example, the leave one out method, one can disable a reaction each time and see the effect. Tne you will be able to identify the essential three reactions.
+
+## Loss funtions
+![loss](./figs/loss_grad.png)
+
+## Profiles
+![exp](./figs/i_exp_1.png)
+![exp](./figs/i_exp_25.png)
--- robertson/rober_crnn.jl
+++ robertson/rober_crnn.jl
@@ -10,21 +10,23 @@ using ProgressBars, Printf
 using Flux.Optimise: update!
 using Flux.Losses: mae, mse
 using BSON: @save, @load
+using LatinHypercubeSampling
 
-is_restart = true;
+is_restart = false;
 n_epoch = 1000000;
 n_plot = 10;
 
 opt = ADAMW(0.005, (0.9, 0.999), 1.f-6);
 datasize = 40;
+batchsize = 16;
 n_exp_train = 20;
-n_exp_val = 10;
+n_exp_val = 5;
 n_exp = n_exp_train + n_exp_val; 
 noise = 1.f-4;
 ns = 3;
-nr = 3;
+nr = 6;
 
-grad_max = 1.e2;
+grad_max = 10 ^ (1);
 maxiters = 10000;
 
 # alg = AutoTsit5(Rosenbrock23(autodiff=false));
@@ -38,19 +40,18 @@ np = nr * (2 * ns + 1) + 1;
 p = (rand(Float32, np) .- 0.5) * 2 * sqrt(6 / (ns + nr));
 p[end] = 1.e-1;
 
-# Generate data sets
-u0_list = rand(Float32, (n_exp, ns)) .+ 0.5;
-u0_list[:, 2:2] .= 0 + lb;
+# Generate datasets
+u0_list = rand(Float32, (n_exp, ns)) .* 2 .+ 0.5;
+u0_list[:, 2:2] .= 0 .+ lb;
+u0_list[:, [1, 3]] .= randomLHC(n_exp, 2) ./ n_exp .+ 0.5
 
 tsteps = 10 .^ range(0, 5, length=datasize);
-# tsteps = range(0, 1e5, length=datasize);
 tspan = Float32[0, tsteps[end]];
 t_end = tsteps[end]
 
 k = [4.f-2, 3.f7, 1.f4];
 ode_data_list = zeros(Float32, (n_exp, ns, datasize));
-std_list = [];
-std_l_ = [];
+yscale_list = [];
 
 function trueODEfunc(dydt, y, k, t)
     r1 = k[1] * y[1]
@@ -72,74 +73,57 @@ for i = 1:n_exp
     u0 = u0_list[i, :]
     prob_trueode = ODEProblem(trueODEfunc, u0, tspan, k)
     ode_data = Array(solve(prob_trueode, alg, saveat=tsteps, atol=atol, rtol=rtol))
-    push!(std_l_, max_min(ode_data))
     ode_data += randn(size(ode_data)) .* ode_data .* noise
     ode_data_list[i, :, :] = ode_data
-    push!(std_list, max_min(ode_data))
+    push!(yscale_list, max_min(ode_data))
 end
 
-y_std = maximum(hcat(std_list...), dims=2);
-dydt_scale = y_std[:, 1] ./ t_end
-show(stdout, "text/plain", round.(y_std', digits=8))
+yscale = maximum(hcat(yscale_list...), dims=2);
+dydt_scale = yscale[:, 1] ./ t_end
+show(stdout, "text/plain", round.(yscale', digits=8))
 
 function p2vec(p)
     slope = abs(p[end])
     w_b = @view(p[1:nr]) .* (10 * slope)
+
     w_in = reshape(@view(p[nr * (ns + 1) + 1:nr * (2 * ns + 1)]), ns, nr)
 
     w_out = reshape(@view(p[nr + 1:nr * (ns + 1)]), ns, nr)
-    w_out = @. -w_in * (10 ^ w_out);
+    w_out = @. -w_in * (10 ^ w_out)
 
-    w_in = clamp.(w_in, 0.f0, 4.f0);
+    w_in = clamp.(w_in, 0, 2.5)
     return w_in, w_b, w_out
 end
 
 function display_p(p)
     w_in, w_b, w_out = p2vec(p)
     println("species (column) reaction (row)")
-    println("w_in")
-    display(w_in')
-    # show(stdout, "text/plain", round.(w_in', digits=3))
-
-    println("w_b")
-    display(w_b')
-    # show(stdout, "text/plain", round.(exp.(w_b'), digits=3))
-
-    println("w_out")
-    display(w_out')
-    # show(stdout, "text/plain", round.(w_out', digits=3))
+    println("w_in | w_b | w_out")
+    display(hcat(w_in', w_b, w_out'))
 
     println("w_out_scale")
     w_out_ = (w_out .* dydt_scale)' .* exp.(w_b)
-    display(w_out_)
-    display(maximum(abs.(w_out_), dims=2)')
+    # display(w_out_)
+    # display(maximum(abs.(w_out_), dims=2)')
     display(w_out_ ./ maximum(abs.(w_out_), dims=2))
-    # show(stdout, "text/plain", round.(w_out' .* exp.(w_b), digits=3))
     println("slope = $(p[end])")
 end
 display_p(p)
 
-function display_grad(grad)
-    grad_w_out = reshape(grad[nr * (ns + 1) + 1:nr * (2 * ns + 1)], ns, nr);
-    println("\ngrad w_out")
-    show(stdout, "text/plain", round.(grad_w_out', digits=6))
-    println("\n")
-
-end
-
 function crnn(du, u, p, t)
-    u .= clamp.(u, lb, Inf)
-    w_in_x = w_in' * @. log(u)
+    w_in_x = w_in' * @. log(clamp(u, lb, Inf))
     du .= w_out * (@. exp(w_in_x + w_b)) .* dydt_scale
 end
 
 u0 = u0_list[1, :]
 prob = ODEProblem(crnn, u0, tspan, saveat=tsteps, atol=atol, rtol=rtol)
 
-sense = BacksolveAdjoint(checkpointing=false; autojacvec=ZygoteVJP());
-function predict_neuralode(u0, p)
+sense = BacksolveAdjoint(checkpointing=true; autojacvec=false);
+function predict_neuralode(u0, p; sample = datasize)
     global w_in, w_b, w_out = p2vec(p)
-    sol = solve(prob, alg, u0=u0, p=p, sensalg=sense, verbose=false, maxiters=maxiters)
+    _prob = remake(prob, tspan=[0, tsteps[sample]])
+    sol = solve(prob, alg, u0=u0, p=p, saveat=tsteps[1:sample], 
+                sensalg=sense, verbose=false, maxiters=maxiters)
     pred = Array(sol)
 
     if sol.retcode == :Success
@@ -147,15 +131,14 @@ function predict_neuralode(u0, p)
     else
         println("ode solver failed")
     end
-
     return pred
 end
-pred = predict_neuralode(u0, p)
+pred = predict_neuralode(u0, p);
 
-function loss_neuralode(p, i_exp)
-    pred = predict_neuralode(u0_list[i_exp, :], p)
+function loss_neuralode(p, i_exp; sample = datasize)
+    pred = predict_neuralode(u0_list[i_exp, :], p; sample)
     ode_data = ode_data_list[i_exp, :, 1:size(pred)[2]]
-    loss = mae(ode_data ./ y_std, pred ./ y_std)
+    loss = mae(ode_data ./ yscale, pred ./ yscale)
     return loss
 end
 loss_neuralode(p, 1)
@@ -173,17 +156,6 @@ cbi = function (p, i_exp)
     plt_all = plot(l_plt..., legend=false)
     png(plt_all, string("figs/i_exp_", i_exp))
 
-    # l_plt = []
-    # for i = 1:ns
-    #     plt = scatter(tsteps, ode_data[i, :], xscale=:identity,
-    #                   markercolor=:transparent, label=string("data_", i))
-    #     plot!(plt, tsteps[1:size(pred)[2]], pred[i, :], xscale=:identity, label=string("pred_", i))
-    #     xlims!(plt, (0, 10))
-    #     push!(l_plt, plt)
-    # end
-    # plt_all = plot(l_plt..., legend=false)
-    # png(plt_all, string("figs/i_exp_zoom_", i_exp))
-
     return false
 end
 
@@ -200,7 +172,7 @@ cb = function (p, loss_train, loss_val, g_norm)
     if iter % n_plot == 0
         display_p(p)
 
-        l_exp = randperm(minimum([n_exp, 10]))[1:1]
+        l_exp = randperm(n_exp)[1:1]
         println("update plot for ", l_exp)
         for i_exp in l_exp
             cbi(p, i_exp)
@@ -213,7 +185,7 @@ cb = function (p, loss_train, loss_val, g_norm)
         xlabel!(plt_grad, "Epoch")
         ylabel!(plt_loss, "Loss")
         ylabel!(plt_grad, "Grad Norm")
-        # ylims!(plt_loss, (-Inf, 1e0))
+        ylims!(plt_loss, (-Inf, 1))
         plt_all = plot([plt_loss, plt_grad]..., legend=:top)
         png(plt_all, "figs/loss_grad")
 
@@ -225,21 +197,17 @@ end
 if is_restart
     @load "./checkpoint/mymodel.bson" p opt l_loss_train l_loss_val l_grad iter
     iter += 1
-    # opt = ADAMW(0.0005, (0.9, 0.999), 1.f-6);
+    # opt = ADAMW(0.001, (0.9, 0.999), 1.f-6)
 end
 
-for i_exp in 1:minimum([n_exp, 10])
-    cbi(p, i_exp)
-end
-
-i_exp = 1;
 epochs = ProgressBar(iter:n_epoch);
 loss_epoch = zeros(Float32, n_exp);
-grad_norm = zeros(Float32, n_exp);
+grad_norm = zeros(Float32, n_exp_train);
 for epoch in epochs
     global p
-    for i_exp in randperm(n_exp)
-        grad = ForwardDiff.gradient(x -> loss_neuralode(x, i_exp), p)
+    for i_exp in randperm(n_exp_train)
+        sample = rand(batchsize:datasize)
+        grad = ForwardDiff.gradient(x -> loss_neuralode(x, i_exp; sample), p)
         grad_norm[i_exp] = norm(grad, 2)
         if grad_norm[i_exp] > grad_max
             grad = grad ./ grad_norm[i_exp] .* grad_max
@@ -258,6 +226,6 @@ end
 
 @printf("min loss train %.4e val %.4e\n", minimum(l_loss_train), minimum(l_loss_val))
 
-for i_exp in 1:minimum([n_exp, 10])
+for i_exp in 1:n_exp
     cbi(p, i_exp)
 end
